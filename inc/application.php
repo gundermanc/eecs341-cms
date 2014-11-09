cc16b757aefad7e0753b9e410abd7869e315b7bb
student <ere16@case.edu>
2014-11-08 23:21:13 -0500

    added login to app

--- inc/application.php
+++ inc/application.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('database.php');
+require_once('util.php');
 
 /**
  * Main Application Context. All application logic above the SQL layer goes
@@ -22,6 +23,19 @@ class Application {
   public static function freshInstall() {
     new Database(true);
   }
-}
 
+  /**
+   * Logs the player in and redirects them to the home page.
+   * Returns false if not accepted
+   */
+  public static function logIn($uname, $pass){
+    if($this->database->authenticateUser($uname,$pass)){
+      logIn($uname);
+      redirectToIndex();
+      return true;
+    } else{
+      return false;
+    }
+  }
+}
 ?>
