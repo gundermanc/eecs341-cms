ff0f21da3af8919c609367ae1b6322de242de68e
student <ere16@case.edu>
2014-11-09 00:21:19 -0500

    more changes to editPage

--- pages/editPage.php
+++ pages/editPage.php
@@ -9,16 +9,26 @@ redirectIfNotLoggedIn();
 $message = "";
 $title="";
 $text="";
+$pid="";
 
-if(isset($_POST['title'])){
+if(isset($_POST['pid'])){
   $title=$_POST['title'];
   $text=$_POST['text'];
-  Application->savePage($title, $text);
+  $pid= $_POST['pid'];
+  try{
+    Application->savePage($pid, $title, $text);
+  } catch(Exception $e){
+    $message=$e->getMessage();
+  }
 }
 
-if(isset($_GET['title'])){
-  $title=$_POST['title'];
-  list($title, $text) = Application->loadPage();
+if(isset($_GET['pid'])){
+  $title=$_GET['pid'];
+  try{
+    list($title, $text) = Application->loadPage();
+  } catch(Exception $e){
+    $message=$e->getMessage();
+  }
 }
 
 ?>
@@ -31,6 +41,7 @@ if(isset($_GET['title'])){
   <form action="editPage.php" method="post">
     <input name="title" type="text"><?php echo $title ?></input>
     <input name="text" type='textArea' id='input'><?php echo $text ?></input>
+    <input name="pid" type="hidden" value="<?php echo $pid ?>">
     <input type=submit></input>
   </form>
   <?php echo $message ?>
