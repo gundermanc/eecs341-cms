194c9e7ce5d1de34118adb715a823d90b5d1241f
Christian Gunderman <cdg46@case.edu>
2014-11-24 15:40:27 -0500

    Login/logout link toggles when logged in.

--- inc/style_engine.php
+++ inc/style_engine.php
@@ -1,16 +1,23 @@
 <?php
 
 require_once('config.php');
+require_once('util.php');
 
 class StyleEngine {
 
- public static function insertHeader($title) {
+ public static function insertHeader($app, $title) {
    global $styleTitle;
+   global $styleApp;
+
    $styleTitle = $title;
+   $styleApp = $app;
    include('header.inc');
  }
 
- public static function insertFooter() {
+ public static function insertFooter($app) {
+   global $styleApp;
+
+   $styleApp = $app;
    include('footer.inc');
  }
 
